7db4af710dd2f7cc342f7b957c0fb96a0274e93a
felipegxavier <felipexavier20015@gmail.com>
2021-01-15 18:05:34 -0300

    add env variables and docker setup

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
