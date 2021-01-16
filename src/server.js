d3f9523db4305bad2d445ca4119779961b2a71c7
felipegxavier <felipexavier20015@gmail.com>
2021-01-16 13:09:03 -0300

    add middleware to serve static files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = require('./rest/routes');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1', routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
 
 app.listen(3000);
